8a8a77fda26860698606163c2d762498f336cb61
James Watling <watlinjame@petit-lyon.ecs.vuw.ac.nz>
2012-08-15 17:08:50 +1200

    Revert "Mouse Movements"
    This reverts commit 203be3998b8d00ca5287931b53534a053584dbe2.

--- juav/WAIUAV/src/TaggableImage.java
+++ juav/WAIUAV/src/TaggableImage.java
@@ -1,20 +1,8 @@
-import java.awt.AWTException;
-import java.awt.Robot;
-
 
 public class TaggableImage {
 	
 	//testing
 	
-	public static void main (String args []) throws AWTException{
-	 
-	Robot robot = new Robot();
-	for (int i =0; i <1920; i++){
-		for (int j =0; j<1080; j++){
-			 robot.mouseMove(i,j);
-		}
-	}
-     robot.mouseMove(300, 550);
-	}
+	
 
 }
