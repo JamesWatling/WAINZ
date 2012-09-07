75549f75459cad9b196afb3a742e239b7c742340
Sam Etheridge <sam@etheridge.co.nz>
2012-09-08 00:51:45 +1200

    format fix

--- juav/WAIUAV/src/ApplicationWindow.java
+++ juav/WAIUAV/src/ApplicationWindow.java
@@ -1,17 +1,9 @@
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.InputStream;
 
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -19,8 +11,6 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSplitPane;
-import javax.swing.JTextArea;
 
 
 public class ApplicationWindow extends JFrame{
@@ -32,66 +22,66 @@ public class ApplicationWindow extends JFrame{
 
 	public ApplicationWindow(){
 		// TODO Auto-generated method stub
-				setLayout(new FlowLayout());
-				//setPreferredSize(new Dimension(800, 600));
-				setResizable(false);
-				JMenuBar menuBar = new JMenuBar();
-				//menuBar
-				
-				JMenu file = new JMenu("File");
-				menuBar.add(file);
-				
-				JMenuItem importItem = new JMenuItem("Import");
-				JMenuItem exportItem = new JMenuItem("Export");
-				JMenuItem quitItem = new JMenuItem("Quit");
-				file.add(importItem);
-				file.add(exportItem);
-				file.add(quitItem);
-				
-				JMenu option = new JMenu("Option");
-				menuBar.add(option);
-				
-				JMenuItem flagItem = new JMenuItem("Flag Image");
-				JMenuItem unflagItem = new JMenuItem("Unflag Image");
-				JMenuItem preferencesItem = new JMenuItem("Preferences");
-				option.add(flagItem);
-				option.add(unflagItem);
-				option.add(preferencesItem);
-				
-				JMenu help = new JMenu("Help");
-				menuBar.add(help);
-				
-				JMenuItem manual = new JMenuItem("Manual");
-				JMenuItem about = new JMenuItem("About");
-				help.add(manual);
-				help.add(about);
-				
-				
-				setJMenuBar(menuBar);
-				
-				JPanel leftPanel = new JPanel();
-				leftPanel.setLayout(new GridLayout(0, 2));
-				for(int i = 0;i<20;i++){
-					JLabel l = new JLabel(new ImageIcon("a.jpg"));
-					l.setBackground(new Color(0, 0, 255));
-					l.setPreferredSize(new Dimension(100, 100));
-					leftPanel.add(l);
-				}
-				JScrollPane leftPane = new JScrollPane(leftPanel);
-				
-				JPanel rightPanel = new JPanel();
-				
+		setLayout(new FlowLayout());
+		//setPreferredSize(new Dimension(800, 600));
+		setResizable(false);
+		JMenuBar menuBar = new JMenuBar();
+		//menuBar
 
-				leftPane.setPreferredSize(new Dimension(250, 600));
-				leftPanel.setBackground(new Color(225, 0, 0));
-				
-				rightPanel.setPreferredSize(new Dimension(550, 600));
-				rightPanel.setBackground(new Color(0, 225, 0));
-				
-				add(leftPane);
-				add(rightPanel);
-				
-				pack();
-				setVisible(true);
+		JMenu file = new JMenu("File");
+		menuBar.add(file);
+
+		JMenuItem importItem = new JMenuItem("Import");
+		JMenuItem exportItem = new JMenuItem("Export");
+		JMenuItem quitItem = new JMenuItem("Quit");
+		file.add(importItem);
+		file.add(exportItem);
+		file.add(quitItem);
+
+		JMenu option = new JMenu("Option");
+		menuBar.add(option);
+
+		JMenuItem flagItem = new JMenuItem("Flag Image");
+		JMenuItem unflagItem = new JMenuItem("Unflag Image");
+		JMenuItem preferencesItem = new JMenuItem("Preferences");
+		option.add(flagItem);
+		option.add(unflagItem);
+		option.add(preferencesItem);
+
+		JMenu help = new JMenu("Help");
+		menuBar.add(help);
+
+		JMenuItem manual = new JMenuItem("Manual");
+		JMenuItem about = new JMenuItem("About");
+		help.add(manual);
+		help.add(about);
+
+
+		setJMenuBar(menuBar);
+
+		JPanel leftPanel = new JPanel();
+		leftPanel.setLayout(new GridLayout(0, 2));
+		for(int i = 0;i<20;i++){
+			JLabel l = new JLabel(new ImageIcon("a.jpg"));
+			l.setBackground(new Color(0, 0, 255));
+			l.setPreferredSize(new Dimension(100, 100));
+			leftPanel.add(l);
+		}
+		JScrollPane leftPane = new JScrollPane(leftPanel);
+
+		JPanel rightPanel = new JPanel();
+
+
+		leftPane.setPreferredSize(new Dimension(250, 600));
+		leftPanel.setBackground(new Color(225, 0, 0));
+
+		rightPanel.setPreferredSize(new Dimension(550, 600));
+		rightPanel.setBackground(new Color(0, 225, 0));
+
+		add(leftPane);
+		add(rightPanel);
+
+		pack();
+		setVisible(true);
 	}
 }
---
+++ juav/WAIUAV/src/images/TaggableImage.java
@@ -0,0 +1,31 @@
+package images;
+import java.awt.image.BufferedImage;
+
+/**
+ * Class containing an image and an associated tag
+ * of the type ImageTag
+ * Images can be tagged as UNTAGGED, NONINFRINGEMENT or INFRINGMENT
+ * @author James McCann (mccannjame), 300192420
+ */
+
+public class TaggableImage {
+	
+	private BufferedImage image;
+	private ImageTag tag;
+	
+	/**
+	 * Constructs a new TaggableImage with an untagged status
+	 * @param image - image stored with this taggable
+	 */
+	public TaggableImage(BufferedImage image){
+		this.image = image;
+		tag = ImageTag.UNTAGGED;
+	}
+	
+	public void setTag(ImageTag newTag) {
+		tag = newTag;
+	}
+	
+	public ImageTag getTag() { return tag; }
+	public BufferedImage getImage() { return image; }
+}
