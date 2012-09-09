7539dc7e40bcb7b3ca91d6bfbb9227e3e8266dac
Sam Etheridge <sam@etheridge.co.nz>
2012-09-09 22:03:04 +1200

    thumbails selectable

--- juav/WAIUAV/src/gui/ImageGridPanel.java
+++ juav/WAIUAV/src/gui/ImageGridPanel.java
@@ -3,20 +3,24 @@ import images.TaggableImage;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JPanel;
 
 
-public class ImageGridPanel extends JPanel {
+public class ImageGridPanel extends JPanel implements MouseListener{
 
 	private static final long serialVersionUID = 1L;
 	private static final Dimension gridPanelSize = new Dimension(100, 100);
 	private List<TaggableImage> images;
+	private List<ImageThumbPanel> imageThumbPanels;
 	//private TaggableImage selectedImage;
 
 	public ImageGridPanel(List<TaggableImage> imageList) {
-		//TODO fill with actual images
+		imageThumbPanels = new ArrayList<ImageThumbPanel>();
 		images = imageList;
 		setLayout(new GridLayout(0, 2));
 		initialise();
@@ -35,11 +39,14 @@ public class ImageGridPanel extends JPanel {
 		System.out.println("initialise imageGrid");
 		ImageThumbPanel itp;
 		if(images != null){
+			imageThumbPanels.clear();
 			for(TaggableImage timg: images){
 				for (int j = 0; j < 20; j++) {
 					//Debug:
 					System.out.println("adding image to grid");
 					itp = new ImageThumbPanel(timg, gridPanelSize);
+					imageThumbPanels.add(itp);
+					itp.addMouseListener(this);
 					add(itp);
 				}
 			}
@@ -47,4 +54,20 @@ public class ImageGridPanel extends JPanel {
 		revalidate();
 		repaint();
 	}
+
+	public void mouseClicked(MouseEvent e) {
+		for(ImageThumbPanel i : imageThumbPanels){
+			System.out.println("b:"+i);
+			i.setSelected(false);
+		}
+		((ImageThumbPanel)e.getSource()).setSelected(true);
+		for(ImageThumbPanel i : imageThumbPanels)
+			System.out.println("a:"+i);
+		repaint();
+		System.out.println();
+	}
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
 }
--- juav/WAIUAV/src/gui/ImageThumbPanel.java
+++ juav/WAIUAV/src/gui/ImageThumbPanel.java
@@ -2,8 +2,10 @@ package gui;
 
 import images.TaggableImage;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -22,10 +24,20 @@ public class ImageThumbPanel extends JPanel {
 	private String fileName;
 	private boolean selected;
 
+	@Override
+	public String toString() {
+		return "ImageThumbPanel [fileName=" + fileName + ", selected="
+				+ selected + "]";
+	}
+
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+
 	public ImageThumbPanel(TaggableImage ti, Dimension dim) {
 		image = ti;//new ImageThumbnail(ti, new Dimension(dim.width-10, dim.height-20));
 		//tag = ti.getTag();
-		fileName = "testing.jpg"; //TODO replace with actual filenames jm 080512
+		fileName = ti.getFileName();
 		initLayout(dim);
 	}
 	
@@ -35,15 +47,25 @@ public class ImageThumbPanel extends JPanel {
 		
 		//image label TODO replace with actual ImageThumbail rendered image
 		JLabel imageLabel = new JLabel(new ImageIcon(image.getImage()));
-		imageLabel.setPreferredSize(new Dimension(dim.width-10, dim.height-20)); //temporary
+		imageLabel.setPreferredSize(new Dimension(dim.width-10, dim.height-30)); //temporary
 		add(imageLabel);
 		
 		//add string and tag below image
 		JLabel nameLabel = new JLabel();
 		nameLabel.setText(fileName);
 		add(nameLabel);
+		
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		if(selected)
+			setBackground(Color.BLUE);
+		else
+			setBackground(null);
+		super.paint(g);
 	}
 	
-	public void toggleSelected() { selected = !selected; }
+	
 
 }
