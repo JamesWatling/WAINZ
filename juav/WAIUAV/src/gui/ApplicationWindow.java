7c7be8520cdc4964bb0cfad4dfef2747ba2fba72
Sam Etheridge <sam@etheridge.co.nz>
2012-10-10 22:30:51 +1300

    fixed exceptions and repeated slow loading when no internet connection

--- juav/WAIUAV/src/gui/ApplicationWindow.java
+++ juav/WAIUAV/src/gui/ApplicationWindow.java
@@ -26,6 +26,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -100,6 +101,8 @@ public class ApplicationWindow extends JFrame implements ActionListener, WindowL
 	private JButton analyzeAllButton;
 	
 	private BufferedImage METADATA_PLACEHOLDER;
+
+	private boolean noConnection = false;
 	public static BufferedImage WAI_LOGO;
 	public static BufferedImage IMPORT_PLACEHOLDER;
 	public static final Color WAI_BLUE = new Color(0, 126, 166);
@@ -597,61 +600,73 @@ public class ApplicationWindow extends JFrame implements ActionListener, WindowL
 		imageMetadataPanel.add(metaDataLabel, BorderLayout.WEST);
 		imageMetadataPanel.setBackground(null);
 		
-	    try {
-			String latitudeS = metaDataLabel.getText().split("GPS Latitude - ", 2)[1].split("</td>", 2)[0];
-			String longitudeS = metaDataLabel.getText().split("GPS Longitude - ")[1].split("</td>",2)[0];
-			
-			Double latitudeNum1 = Double.parseDouble(latitudeS.split((char) 0x00B0+"", 2)[0]);
-			Double latitudeNum2 = Double.parseDouble(latitudeS.split((char) 0x00B0+"", 2)[1].split("' ", 2)[0]);
-			Double latitudeNum3 = Double.parseDouble(latitudeS.split("' ", 2)[1].split("\"", 2)[0]);
-			
-			Double longitudeNum1 = Double.parseDouble(longitudeS.split((char) 0x00B0+"", 2)[0]);
-			Double longitudeNum2 = Double.parseDouble(longitudeS.split((char) 0x00B0+"", 2)[1].split("' ", 2)[0]);
-			Double longitudeNum3 = Double.parseDouble(longitudeS.split("' ", 2)[1].split("\"", 2)[0]);
-			
-			Double latitude; 
-			Double longitude;
-			
-			if(latitudeNum1>=0)
-				latitude = latitudeNum1+latitudeNum2/60+latitudeNum3/3600;
-			else 
-				latitude = latitudeNum1-latitudeNum2/60-latitudeNum3/3600;
-			
-			if(longitudeNum1>=0)
-				longitude= longitudeNum1+longitudeNum2/60+longitudeNum3/3600;
-			else
-				longitude= longitudeNum1-longitudeNum2/60-longitudeNum3/3600;
-			
-			URLConnection con = null;
-			if(useProxy)
-				con = new URL("http",proxyUrl,proxyPort,"http://maps.google.com/maps/api/staticmap?" +
-					"center="+latitude+",%20"+longitude +
-					"&zoom=7&size="
-					+IMAGE_METADATA_PANEL_SIZE.width/3+"x"+
-					IMAGE_METADATA_PANEL_SIZE.height+
-					"&maptype=roadmap&sensor=false&" +
-					"markers=||"+latitude+",%20"+longitude).openConnection();
-			else
-				con = new URL("http://maps.google.com/maps/api/staticmap?" +
+	    
+			try {
+				String latitudeS = metaDataLabel.getText().split("GPS Latitude - ", 2)[1].split("</td>", 2)[0];
+				String longitudeS = metaDataLabel.getText().split("GPS Longitude - ")[1].split("</td>",2)[0];
+				
+				Double latitudeNum1 = Double.parseDouble(latitudeS.split((char) 0x00B0+"", 2)[0]);
+				Double latitudeNum2 = Double.parseDouble(latitudeS.split((char) 0x00B0+"", 2)[1].split("' ", 2)[0]);
+				Double latitudeNum3 = Double.parseDouble(latitudeS.split("' ", 2)[1].split("\"", 2)[0]);
+				
+				Double longitudeNum1 = Double.parseDouble(longitudeS.split((char) 0x00B0+"", 2)[0]);
+				Double longitudeNum2 = Double.parseDouble(longitudeS.split((char) 0x00B0+"", 2)[1].split("' ", 2)[0]);
+				Double longitudeNum3 = Double.parseDouble(longitudeS.split("' ", 2)[1].split("\"", 2)[0]);
+				
+				Double latitude; 
+				Double longitude;
+				
+				if(latitudeNum1>=0)
+					latitude = latitudeNum1+latitudeNum2/60+latitudeNum3/3600;
+				else 
+					latitude = latitudeNum1-latitudeNum2/60-latitudeNum3/3600;
+				
+				if(longitudeNum1>=0)
+					longitude= longitudeNum1+longitudeNum2/60+longitudeNum3/3600;
+				else
+					longitude= longitudeNum1-longitudeNum2/60-longitudeNum3/3600;
+				
+				URLConnection con = null;
+				if(noConnection );
+				else if(useProxy)
+					con = new URL("http",proxyUrl,proxyPort,"http://maps.google.com/maps/api/staticmap?" +
 						"center="+latitude+",%20"+longitude +
 						"&zoom=7&size="
 						+IMAGE_METADATA_PANEL_SIZE.width/3+"x"+
 						IMAGE_METADATA_PANEL_SIZE.height+
 						"&maptype=roadmap&sensor=false&" +
 						"markers=||"+latitude+",%20"+longitude).openConnection();
-			InputStream is = con.getInputStream();
-			byte bytes[] = new byte[con.getContentLength()];
-			Toolkit tk = getToolkit();
-			BufferedImage map = ImageIO.read(is);
-			tk.prepareImage(map, -1, -1, null);
-			imageMetadataPanel.add(new JLabel(new ImageIcon(map)),BorderLayout.EAST);
-		}
-	    catch (Exception e1) {
-	    	e1.printStackTrace();
-	    	JLabel errorLabel = new JLabel("<html><p>No Internet connection</p><p>Or no image metadata.</p></html>");
-	    	errorLabel.setVerticalAlignment(JLabel.TOP);
-			imageMetadataPanel.add(errorLabel,BorderLayout.EAST);
-		}
+				else
+					con = new URL("http://maps.google.com/maps/api/staticmap?" +
+							"center="+latitude+",%20"+longitude +
+							"&zoom=7&size="
+							+IMAGE_METADATA_PANEL_SIZE.width/3+"x"+
+							IMAGE_METADATA_PANEL_SIZE.height+
+							"&maptype=roadmap&sensor=false&" +
+							"markers=||"+latitude+",%20"+longitude).openConnection();
+				InputStream is = con.getInputStream();
+				byte bytes[] = new byte[con.getContentLength()];
+				Toolkit tk = getToolkit();
+				BufferedImage map = ImageIO.read(is);
+				tk.prepareImage(map, -1, -1, null);
+				imageMetadataPanel.add(new JLabel(new ImageIcon(map)),BorderLayout.EAST);
+			} catch (IOException e1) {
+		    	//e1.printStackTrace();
+		    	JLabel errorLabel = new JLabel("<html><p>No Internet connection</p></html>");
+		    	errorLabel.setVerticalAlignment(JLabel.TOP);
+				imageMetadataPanel.add(errorLabel,BorderLayout.EAST);
+				noConnection = true;
+			} catch (NullPointerException e1) {
+		    	//e1.printStackTrace();
+		    	JLabel errorLabel = new JLabel("<html><p>No Internet connection</p></html>");
+		    	errorLabel.setVerticalAlignment(JLabel.TOP);
+				imageMetadataPanel.add(errorLabel,BorderLayout.EAST);
+			} catch (Exception e1){
+				//e1.printStackTrace();
+		    	JLabel errorLabel = new JLabel("<html><p>No Internet connection </p><p>or no valid metadata</p></html>");
+		    	errorLabel.setVerticalAlignment(JLabel.TOP);
+				imageMetadataPanel.add(errorLabel,BorderLayout.EAST);
+			}
 		
 	    repaint();
 	}
@@ -662,6 +677,7 @@ public class ApplicationWindow extends JFrame implements ActionListener, WindowL
 	public void mouseExited(MouseEvent e) {}
 	
 	public void checkSetting() {
+		noConnection = false;
 		Scanner sc;
 		try {
 			sc = new Scanner(new File("settings.data"));
